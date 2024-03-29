feaeb8458c6099c8b7a106c43567fbfd585c0089
rpanchyk <ruslan.panchik@gmail.com>
2024-03-29 08:49:58 +0200

    Add N001_TwoSum

---
+++ src/main/java/com/leetcode/solutions/N001_TwoSum.java
@@ -0,0 +1,25 @@
+package com.leetcode.solutions;
+
+import java.util.Arrays;
+
+public class N001_TwoSum {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new N001_TwoSum().twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(new N001_TwoSum().twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(new N001_TwoSum().twoSum(new int[]{3, 3}, 6)));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums != null && nums.length > 1) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
