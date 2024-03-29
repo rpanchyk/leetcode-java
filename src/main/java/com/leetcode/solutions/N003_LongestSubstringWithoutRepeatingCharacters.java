22b7971bc746db0884d4c32b2b26de8a89dcb3b1
rpanchyk <ruslan.panchik@gmail.com>
2024-03-29 08:54:29 +0200

    Add N003_LongestSubstringWithoutRepeatingCharacters

---
+++ src/main/java/com/leetcode/solutions/N003_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,41 @@
+package com.leetcode.solutions;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class N003_LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        System.out.println(new N003_LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(new N003_LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("bbbbb"));
+        System.out.println(new N003_LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("pwwkew"));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        List<String> list = new ArrayList<>();
+        int result = 0;
+
+        for (String c : s.split("")) {
+            if (list.contains(c)) {
+                Iterator<String> it = list.iterator();
+                while (it.hasNext()) {
+                    String n = it.next();
+                    it.remove();
+                    if (n.equals(c)) {
+                        break;
+                    }
+                }
+            }
+            list.add(c);
+
+            int size = list.size();
+            result = Math.max(size, result);
+        }
+        return result;
+    }
+}
