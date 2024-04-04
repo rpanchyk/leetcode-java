2df59b0db9d2632c9fe6b91a6edb974b52c2deba
rpanchyk <ruslan.panchik@gmail.com>
2024-04-04 09:30:43 +0300

    Update AssertUtils

--- src/main/java/com/leetcode/utils/AssertUtils.java
+++ src/main/java/com/leetcode/utils/AssertUtils.java
@@ -1,12 +1,26 @@
 package com.leetcode.utils;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class AssertUtils {
 
     static int counter = 0;
 
     public static void assertThat(Object actual, Object expected) {
+        if (actual != null && actual.getClass().isArray()) {
+            if (actual.getClass().getComponentType() == int.class) {
+                actual = Arrays.toString((int[]) actual);
+            }
+        }
+        if (expected != null && expected.getClass().isArray()) {
+            if (expected.getClass().getComponentType() == int.class) {
+                expected = Arrays.toString((int[]) expected);
+            }
+        }
+
         System.out.println(++counter + " => " + actual);
-        if (!actual.equals(expected)) {
+        if (!Objects.equals(actual, expected)) {
             throw new RuntimeException("Actual '" + actual + "', but expected '" + expected + "' :(");
         }
     }
